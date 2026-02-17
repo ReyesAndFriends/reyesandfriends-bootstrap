9df8002fcba57c6d48659349602d8b762647d137
Marcos Reyes <astronautmarkus@gmail.com>
2026-02-17 20:53:33 -0300

    fix: se importa el archivo faltante de API Preload para postgreSQL para su funcionamiento en render.

--- reyesandfriends-bootstrap/src/preload/index.ts
+++ reyesandfriends-bootstrap/src/preload/index.ts
@@ -1,8 +1,11 @@
 import { electronAPI } from "@electron-toolkit/preload";
+
 import "./databases/mysql/mysqlAPI";
+import "./databases/postgresql/postgresqlAPI";
 import "./http-servers/apache/apacheAPI";
 import "./http-servers/nginx/nginxAPI";
 import "./settings/settingsAPI";
 
 import { contextBridge } from "electron";
+
 contextBridge.exposeInMainWorld("electron", electronAPI);
